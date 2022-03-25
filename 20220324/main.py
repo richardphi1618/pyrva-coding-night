ba2088a3682bfb4d81d8ba4d3796aa3a827f17c5
Richard Blanchette <richardphi1618@gmail.com>
2022-03-24 20:03:34 -0400

    feat(20220324): end of night

--- 20220324/main.py
+++ 20220324/main.py
@@ -1,14 +1,63 @@
+from datetime import datetime
+
 import sqlalchemy as db
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
 engine = db.create_engine("sqlite:///example_data.db")
 connection = engine.connect()
 metadata = db.MetaData()
-ssh_logins = db.Table("logins", metadata, autoload=True, autoload_with=engine)
+Base = declarative_base()
+logins = db.Table("logins", metadata, autoload=True, autoload_with=engine)
 # Print the column names
-print(ssh_logins.columns.keys())
+print(logins.columns.keys())
 # Print full table metadata
 print(repr(metadata.tables["logins"]))
 
 
-select * from example_data.logins
-limit 100
+class logins_table(Base):
+    __tablename__ = "logins"  # redundant
+
+    month = Column("Month", String, primary_key=True)
+    day = Column("DayOftheMonth", String, primary_key=True)
+    time = Column("Time", String, primary_key=True)
+    username = Column("Username", String, primary_key=True)
+    ip = Column("IPAddress", String)
+    port = Column("Port", Integer)
+
+    def __repr__(self):
+        return f"User(month={self.month!r}, day={self.day!r}, time={self.time!r}, \
+                    username={self.username!r}, ip={self.ip!r}, port={self.port!r} )"
+
+    def date(self):
+        time_parts = [int(part) for part in repr(self.time).split(":")]
+        return datetime(2022, int(repr(self.month)), int(repr(self.day)))
+
+
+# select * from example_data.logins
+# limit 10
+stmt1 = db.select(logins_table).limit(10)
+result = connection.execute(stmt1)
+print(f"\n~~~~~~~~\n{result}\n~~~~~~~~\n")
+
+print(f"\n~~~~~~~~\n")
+for i in connection.execute(stmt1):
+    print(i)
+print(f"\n~~~~~~~~\n")
+
+
+# # generator tutorial
+# def fib():
+#     a = 0
+#     b = 1
+#     while True:
+#         c = a + b
+#         a, b = b, c
+#         yield c
+
+
+# for num in fib():
+#     print(f"I got {num}")
+#     if num > 100:
+#         break
+# #~~~~~~~~~~~~~~~~~~~
--- 20220324/readme.md
+++ 20220324/readme.md
@@ -6,8 +6,12 @@
   - [docs](https://docs.sqlalchemy.org/en/14/)
 - [kaggle](https://www.kaggle.com/) 
 - [sqlite-utils](https://github.com/simonw/sqlite-utils) 
+  - cli example of csv conversion to db: ```sqlite-utils insert example_data.db logins .\data\csv\ssh_login_attempts.csv --csv```
 
 ## notes:
 
-[kaggle data set](https://www.kaggle.com/datasets/booroom/ssh-login-attempts-on-my-raspberry-pi)
-[tutorial](https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91)
+- [kaggle data set](https://www.kaggle.com/datasets/booroom/ssh-login-attempts-on-my-raspberry-pi)
+- [tutorial](https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91)
+
+
+- [!r](https://stackoverflow.com/questions/44800801/in-python-format-f-string-strings-what-does-r-mean)
